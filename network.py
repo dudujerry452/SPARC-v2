769c540648c4d4198d0008e1a7109eabdf3d5d46
dudujerry452 <dudujerry452@qq.com>
2026-06-16 10:57:10 +0800

    phase2

--- network.py
+++ network.py
@@ -53,6 +53,40 @@ class LTE(nn.Module):
 
     return Q, K, V
 
+class LTEVGG(nn.Module): 
+    def __init__(self, in_ch=1): 
+      super().__init__() 
+      self.conv1 = nn.Sequential(
+         nn.Conv2d(in_ch, 64, 3, 1, 1), 
+         nn.ReLU(), 
+         nn.Conv2d(64, 64, 3, 1, 1), 
+         nn.ReLU(), 
+      )
+
+      self.conv2 = nn.Sequential(
+         nn.MaxPool2d(2,2), 
+         nn.Conv2d(64, 128, 3, 1, 1), 
+         nn.ReLU(), 
+         nn.Conv2d(128, 128, 3, 1, 1), 
+         nn.ReLU(), 
+      )
+
+      self.conv3 = nn.Sequential(
+         nn.MaxPool2d(2,2), 
+         nn.Conv2d(128, 256, 3, 1, 1), 
+         nn.ReLU(), 
+         nn.Conv2d(256, 256, 3, 1, 1), 
+         nn.ReLU(), 
+      )
+
+    def forward(self, x): 
+        x1 = self.conv1(x) 
+        x2 = self.conv2(x1) 
+        x3 = self.conv3(x2)
+
+        return x1, x2, x3 
+
+
 class RelianceEmbedding(nn.Module): 
   """
   相关性嵌入
@@ -113,6 +147,38 @@ class Backbone(nn.Module):
     def forward(self, lr):
         return self.body(lr)   # (B, C, H, W)
     
+class ResBlock(nn.Module): 
+    def __init__(self, feat_ch=64, res_scale=1.0): 
+      super().__init__()
+      self.scale = res_scale
+      self.body = nn.Sequential(
+         nn.Conv2d(feat_ch, feat_ch, 3, 1, 1), 
+         nn.ReLU(), 
+         nn.Conv2d(feat_ch, feat_ch, 3, 1, 1), 
+      )
+    def forward(self, x): 
+       res = self.body(x) 
+       return x + res * self.scale
+    
+class SFE(nn.Module): 
+    def __init__(self, in_ch=1, feat_ch=64, res_scale=1.0): 
+        super().__init__() 
+        self.scale = res_scale 
+        self.conv_head = nn.Conv2d(in_ch, feat_ch, 3, 1, 1) 
+        self.resblk = nn.Sequential(
+            ResBlock(feat_ch), 
+            ResBlock(feat_ch), 
+            ResBlock(feat_ch) 
+        )
+        self.conv_tail = nn.Conv2d(feat_ch, feat_ch, 3, 1, 1) 
+    
+    def forward(self, x): 
+        x = self.conv_head(x) 
+        res = self.resblk(x) 
+        res = self.conv_tail(res) 
+        return x + res * self.scale 
+   
+
 class SoftAttention(nn.Module): 
   """
   F: (B, C, Hl, Wl)
@@ -122,14 +188,13 @@ class SoftAttention(nn.Module):
   def __init__(self, feat_ch=64): 
     super().__init__() 
     self.fusion_conv = nn.Sequential(
-      nn.Conv2d(2*feat_ch, feat_ch, 3, 1, 1), 
+      nn.Conv2d(feat_ch+ 256, feat_ch, 3, 1, 1),  # 256 from VGG19
       nn.ReLU()
     )
 
   def forward(self, F, T, S): 
     B, C, H, W = F.shape
-    T = T.view(B, C, H, W) 
-    S = S.view(B, 1, H, W)
+    # print(F.shape, T.shape, S.shape)
 
     fused = torch.cat([F, T], dim=1) # concat them with channel dimension (B, 2*C, H, W)
     fused = self.fusion_conv(fused)  # fuse them (B, C, H, W)
@@ -206,21 +271,41 @@ class TTSR(nn.Module):
     super().__init__()
     self.name = "TTSR-basic"
 
-    self.LTE = LTE(feat_ch)
+    self.LTEVGG = LTEVGG(in_ch)
     self.RE = RelianceEmbedding(kernel_size=3)
     self.HA = HardAttention()
     self.SA = SoftAttention(feat_ch)
-    self.BB = Backbone(in_ch, feat_ch)
+    self.Backbone = SFE(in_ch, feat_ch)
     self.OL = OutputLayer(in_ch, feat_ch, scale=(4,1)) 
   def forward(self, lr, ref): 
-    Hl, Wl, Hr, Wr = lr.shape[2], lr.shape[3], ref.shape[2], ref.shape[3]
 
-    Q, K, V = self.LTE(lr, ref, target_size=(Hl, Wl)) # K, V: (B, C, Hr, Wr), Q = 
-    H, S = self.RE(Q, K)
-    T = self.HA(H, V)  
-    F = self.BB(lr)
+    B, _, H, W = lr.shape 
+    _, _, H1, W1 = ref.shape 
+    scale_h = H / H1
+    scale_w = W / W1 
+
+    lr_up = F.interpolate(lr, size=(H1, W1), mode="bicubic", align_corners=False)
+    ref_down = F.interpolate(ref, scale_factor=(scale_h, scale_w), mode="bicubic", align_corners=False)
+    ref_down_up = F.interpolate(ref_down, size=(H1, W1), mode="bicubic", align_corners=False)
+
+    lrsr_v1, lrsr_v2, lrsr_v3 = self.LTEVGG(lr_up) # v1, v2, v3: /1, /2, /4
+    refsr_v1, refsr_v2, refsr_v3 = self.LTEVGG(ref_down_up) 
+    ref_v1, ref_v2, ref_v3 = self.LTEVGG(ref) 
+
+    Q, K, V = lrsr_v3, refsr_v3, ref_v3 # (B, C, H1/4, W1/4)
+
+    H, S = self.RE(Q, K) # (B, N_q = H1*W1/16)
+    T = self.HA(H, V)  # (B, N_q)
+
+    S = S.view(B, -1, V.shape[2], V.shape[3]) # (B, C, H1/4, W1/4)
+    T = T.view(B, -1, V.shape[2], V.shape[3]) 
+    S = F.interpolate(S, size=(lr.shape[2], lr.shape[3]), align_corners=False, mode="bicubic")
+    T = F.interpolate(T, size=(lr.shape[2], lr.shape[3]), align_corners=False, mode="bicubic")
+
+
+    F_ = self.Backbone(lr) # (B, C, H, W)
     
-    output = self.SA(F, T, S) 
+    output = self.SA(F_, T, S) 
 
     # send_tensor("out", output[0][0], f"TTSR top output")
 
